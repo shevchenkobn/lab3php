a8fbc2d06e169d45add9c61ba7e8236edb618796
shevchenkobn <bohdan.shevchenko1@nure.ua>
2017-05-15 06:46:03 +0300

    almost done

--- templates/header.php
+++ templates/header.php
@@ -9,15 +9,11 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <!--<link rel="stylesheet" href="<?=dirname($_SERVER["PHP_SELF"])?>/css/bootstrap.min.css">
-        <script src="<?=dirname($_SERVER["PHP_SELF"])?>/js/bootstrap.min.js"></script>
-        <script src="<?=dirname($_SERVER["PHP_SELF"])?>/js/jquery.min.js"></script>-->
-
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-        <link rel="stylesheet" href="<?=dirname($_SERVER["PHP_SELF"])?>/css/styles.css">
+        <link rel="stylesheet" href="<?php echo dirname($_SERVER["PHP_SELF"])?>/css/styles.css">
         <script src="<?php echo dirname($_SERVER["PHP_SELF"])?>/js/script.js"></script>
 
         <meta name="viewport" content="width=device-width, initial-scale=1">
