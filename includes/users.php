b9ea79e170173b6bf6b6f5c715cf0ca2002bce67
shevchenkobn <bohdan.shevchenko1@nure.ua>
2017-05-23 14:04:25 +0300

    profile editing implementation in process

--- includes/users.php
+++ includes/users.php
@@ -14,18 +14,27 @@ class user
     public $surname;  //strlen >= 1
     public $age; // int!!!, > 16
     public $description; //strlen >= 50
-    function __construct($name, $password, $description)
+    function __construct($login, $password, $name, $surname, $age, $description)
     {
-        if (!is_string($name))
+        if (!is_string($login))
+            throw new InvalidArgumentException("Wrong login");
+        $this->login = $login;
+        if (!is_string($name) || strlen($name) < 2)
             throw new InvalidArgumentException("Wrong name");
+        $this->name = $name;
+        if (!is_string($surname) || strlen($surname) < 2)
+            throw new InvalidArgumentException("Wrong surname");
+        $this->surname = $surname;
+        if (!is_int($age) || $age < 16)
+            throw new InvalidArgumentException("Wrong age");
         $this->name = $name;
         if (!is_string($password))
             throw new InvalidArgumentException("Wrong password");
         $this->pass = $password;
-        if (!is_string($description))
-            throw new InvalidArgumentException("Wrong password");
+        if (!is_string($description) || strlen($surname) < 50)
+            throw new InvalidArgumentException("Wrong description");
         $this->description = $description;
     }
 }
 
-return array(new user("Bogdan", "123", "This is my account"));
+return array(new user("bogdan222", "123", "Bogdan", "SH",132,"This is my account"));
